39ea43b011b1f89d4bb3cf8ffac96a9165fa8910
Podvolotskiy Arkadiy <Poarig@yandex.ru>
2021-04-13 17:54:36 +0300

    Починил первый файл

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
 # Импортируем необходимые классы.
-from telegram.ext import Updater
-# TOKEN =
+from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import CallbackContext, CommandHandler
+TOKEN = "1761635851:AAFkWG9yyhCoKtwpjuEwKth8Fp6QC_sQsvY"
+REQUEST_KWARGS = {
+    'proxy_url': 'socks4://171.103.9.22:4145/',
+    # Optional, if you need authentication:
+    'urllib3_proxy_kwargs': {
+        'assert_hostname': 'False',
+        'cert_reqs': 'CERT_NONE'
+        # 'username': 'user',
+        # 'password': 'password'
+    }
+}
+
+
+# Определяем функцию-обработчик сообщений.
+# У неё два параметра, сам бот и класс updater, принявший сообщение.
+def echo(update, context):
+    # У объекта класса Updater есть поле message,
+    # являющееся объектом сообщения.
+    # У message есть поле text, содержащее текст полученного сообщения,
+    # а также метод reply_text(str),
+    # отсылающий ответ пользователю, от которого получено сообщение.
+    update.message.reply_text(update.message.text)
 
 
 def main():
     # Создаём объект updater.
     # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
-    updater = Updater(TOKEN, use_context=True)
+    updater = Updater(TOKEN, use_context=True,
+                      request_kwargs=REQUEST_KWARGS)
 
     # Получаем из него диспетчер сообщений.
     dp = updater.dispatcher
 
+    # Создаём обработчик сообщений типа Filters.text
+    # из описанной выше функции echo()
+    # После регистрации обработчика в диспетчере
+    # эта функция будет вызываться при получении сообщения
+    # с типом "текст", т. е. текстовых сообщений.
+    text_handler = MessageHandler(Filters.text, echo)
+
+    # Регистрируем обработчик в диспетчере.
+    dp.add_handler(text_handler)
     # Запускаем цикл приема и обработки сообщений.
     updater.start_polling()
 
