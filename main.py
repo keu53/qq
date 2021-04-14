5cde1e2125f379d835f05be772f4f9b35be2942a
4260snow <irbis314@yandex.ru>
2021-04-14 18:50:57 +0300

    Рабочий proxy

--- main.py
+++ main.py
@@ -1,49 +1,33 @@
-# Импортируем необходимые классы.
+import logging
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CallbackContext, CommandHandler
+
+
 TOKEN = "1761635851:AAFkWG9yyhCoKtwpjuEwKth8Fp6QC_sQsvY"
 REQUEST_KWARGS = {
-    'proxy_url': 'socks4://171.103.9.22:4145/',
-    # Optional, if you need authentication:
+    'proxy_url': 'socks5://t3.learn.python.ru:1080',    # t3 можно менять на t1 или t2
     'urllib3_proxy_kwargs': {
         'assert_hostname': 'False',
-        'cert_reqs': 'CERT_NONE'
-        # 'username': 'user',
-        # 'password': 'password'
+        'cert_reqs': 'CERT_NONE',
+        'username': 'learn',
+        'password': 'python'
     }
 }
+logging.captureWarnings(True)
 
-
-# Определяем функцию-обработчик сообщений.
-# У неё два параметра, сам бот и класс updater, принявший сообщение.
 def echo(update, context):
-    # У объекта класса Updater есть поле message,
-    # являющееся объектом сообщения.
-    # У message есть поле text, содержащее текст полученного сообщения,
-    # а также метод reply_text(str),
-    # отсылающий ответ пользователю, от которого получено сообщение.
     update.message.reply_text(update.message.text)
 
 
 def main():
-    # Создаём объект updater.
-    # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
     updater = Updater(TOKEN, use_context=True,
                       request_kwargs=REQUEST_KWARGS)
 
-    # Получаем из него диспетчер сообщений.
     dp = updater.dispatcher
-
-    # Создаём обработчик сообщений типа Filters.text
-    # из описанной выше функции echo()
-    # После регистрации обработчика в диспетчере
-    # эта функция будет вызываться при получении сообщения
-    # с типом "текст", т. е. текстовых сообщений.
     text_handler = MessageHandler(Filters.text, echo)
 
-    # Регистрируем обработчик в диспетчере.
     dp.add_handler(text_handler)
-    # Запускаем цикл приема и обработки сообщений.
+
     updater.start_polling()
 
     # Ждём завершения приложения.
@@ -51,6 +35,5 @@ def main():
     updater.idle()
 
 
-# Запускаем функцию main() в случае запуска скрипта.
 if __name__ == '__main__':
     main()
